8e1b9896f8629e5bd551bdde82b7ad2ea3d29731
Simon-Oliver <stauffer.simon@outlook.com>
2018-10-01 09:26:35 +1000

    First Commit

---
+++ Javascript Lessions/script.js
@@ -0,0 +1,12 @@
+const number = Number(prompt('Guess a number between 0 and 10?'));
+const target = Math.floor(Math.random() * 11);
+
+if (number === target) {
+  console.log('BANG ON !!!');
+} else if (number < target) {
+  console.log('To low!');
+} else if (number > target) {
+  console.log('To High!!!');
+}
+
+console.log(target);
