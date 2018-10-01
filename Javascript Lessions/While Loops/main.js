c3533a9d7b58fd82f5e8d82f666d57a0d220a809
Simon-Oliver <stauffer.simon@outlook.com>
2018-10-01 10:11:27 +1000

    Small changes

---
+++ Javascript Lessions/While Loops/main.js
@@ -0,0 +1,37 @@
+{
+  let index = -10;
+  while (index <= 19) {
+    console.log(index);
+    index++;
+  }
+}
+
+{
+  let index = 10;
+  while (index <= 40) {
+    if (index % 2 === 0) {
+      console.log(index);
+    }
+    index++;
+  }
+}
+
+{
+  let index = 300;
+  while (index <= 333) {
+    if (index % 2 !== 0) {
+      console.log(index);
+    }
+    index++;
+  }
+}
+
+{
+  let index = 5;
+  while (index <= 50) {
+    if (index % 5 === 0 && index % 3 === 0) {
+      console.log(index);
+    }
+    index++;
+  }
+}
