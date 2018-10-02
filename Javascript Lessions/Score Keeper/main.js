68c99c1640f0ec8acf41b1995f37f8baa279b1bb
Simon-Oliver <stauffer.simon@outlook.com>
2018-10-02 20:56:05 +1000

    Small refactoring

--- Javascript Lessions/Score Keeper/main.js
+++ Javascript Lessions/Score Keeper/main.js
@@ -2,11 +2,29 @@ const p1Button = document.querySelector('#p1');
 const p2Button = document.querySelector('#p2');
 const scoreOne = document.querySelector('#p1Display');
 const scoreTwo = document.querySelector('#p2Display');
+const numInput = document.querySelector('input');
+const playingTo = document.querySelector('#playingTo');
+
 const reset = document.querySelector('#reset');
+
+function resetFunc() {
+  p1 = 0;
+  p2 = 0;
+  scoreOne.textContent = p1;
+  scoreTwo.textContent = p2;
+  scoreOne.classList.remove('winner');
+  scoreTwo.classList.remove('winner');
+  gameOver = false;
+}
+
 let p1 = 0;
 let p2 = 0;
 let gameOver = false;
-const winningScore = 5;
+let winningScore = 5;
+
+numInput.addEventListener('change', () => {
+  winningScore = Number(numInput.textContent);
+});
 
 p1Button.addEventListener('click', () => {
   if (!gameOver) {
@@ -31,11 +49,11 @@ p2Button.addEventListener('click', () => {
 });
 
 reset.addEventListener('click', () => {
-  p1 = 0;
-  p2 = 0;
-  scoreOne.textContent = p1;
-  scoreTwo.textContent = p2;
-  scoreOne.classList.remove('winner');
-  scoreTwo.classList.remove('winner');
-  gameOver = false;
+  resetFunc();
+});
+
+numInput.addEventListener('change', () => {
+  playingTo.textContent = numInput.value;
+  winningScore = Number(numInput.value);
+  resetFunc();
 });
