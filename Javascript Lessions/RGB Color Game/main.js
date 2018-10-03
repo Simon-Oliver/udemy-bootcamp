5b2116d43cd327630476b4f7aedf42e0eabf1f46
Simon-Oliver <stauffer.simon@outlook.com>
2018-10-04 00:11:27 +1000

    Added more functionality and added comments

--- Javascript Lessions/RGB Color Game/main.js
+++ Javascript Lessions/RGB Color Game/main.js
@@ -8,16 +8,34 @@ const colors = [
 ];
 
 const colorDisplay = document.querySelector('#colorDisplay');
+const header = document.querySelector('h1');
+const message = document.querySelector('#message');
 const squares = document.querySelectorAll('.square');
 const pickedColor = colors[3];
 
-squares.forEach((e, i, arg) => {
+// Function will change all squares to the winning color
+function changeColor(color) {
+  squares.forEach(e => {
+    e.style.backgroundColor = color;
+  });
+  header.style.backgroundColor = color;
+}
+
+squares.forEach((e, i) => {
   // Add inital colors
   e.style.backgroundColor = colors[i];
-  // Add eventListener
+  // Add eventListener for each square
   e.addEventListener('click', () => {
-    alert(arg[i].style.backgroundColor);
+    const selected = e.style.backgroundColor;
+    // check if selected color matches winning color
+    if (selected === pickedColor) {
+      colorDisplay.textContent = pickedColor;
+      message.textContent = 'Correct!!!';
+      changeColor(e.style.backgroundColor);
+      // if colors don't match square will "disappear"
+    } else {
+      e.style.backgroundColor = '#23232323';
+      message.textContent = 'Try Again!';
+    }
   });
-}, squares);
-
-colorDisplay.textContent = pickedColor;
+});
