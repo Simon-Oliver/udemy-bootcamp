cf4a9e20798124e1be9f731ae9b31e1a7bf3a32b
Simon-Oliver <stauffer.simon@outlook.com>
2018-10-04 19:34:04 +1000

    More functionality

--- Javascript Lessions/RGB Color Game/main.js
+++ Javascript Lessions/RGB Color Game/main.js
@@ -1,17 +1,10 @@
-const colors = [
-  'rgb(255, 0, 0)',
-  'rgb(255, 255, 0)',
-  'rgb(0, 255, 0)',
-  'rgb(0, 255, 255)',
-  'rgb(0, 0, 255)',
-  'rgb(255, 0, 255)'
-];
+const colors = generateColor(6);
 
 const colorDisplay = document.querySelector('#colorDisplay');
 const header = document.querySelector('h1');
 const message = document.querySelector('#message');
 const squares = document.querySelectorAll('.square');
-const pickedColor = colors[3];
+const pickedColor = pickColor();
 
 // Function will change all squares to the winning color
 function changeColor(color) {
@@ -21,6 +14,31 @@ function changeColor(color) {
   header.style.backgroundColor = color;
 }
 
+function pickColor() {
+  const random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateColor(num) {
+  const arr = [];
+
+  for (let i = 0; num > i; i++) {
+    arr.push(randomColor());
+  }
+
+  return arr;
+}
+
+function randomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const RGB = `rgb(${r}, ${g}, ${b})`;
+  return RGB;
+}
+
+colorDisplay.textContent = pickedColor;
+
 squares.forEach((e, i) => {
   // Add inital colors
   e.style.backgroundColor = colors[i];
@@ -29,7 +47,6 @@ squares.forEach((e, i) => {
     const selected = e.style.backgroundColor;
     // check if selected color matches winning color
     if (selected === pickedColor) {
-      colorDisplay.textContent = pickedColor;
       message.textContent = 'Correct!!!';
       changeColor(e.style.backgroundColor);
       // if colors don't match square will "disappear"
