ea710a5606361e273e56bc28ae2e32e3a22f96c2
Simon-Oliver <stauffer.simon@outlook.com>
2018-10-09 22:59:35 +1100

    Added some functionality

--- Javascript Lessions/ToDo/Assets/js/main.js
+++ Javascript Lessions/ToDo/Assets/js/main.js
@@ -1,1 +1,32 @@
-console.log('Hello');
+$('li').click(strikeOut);
+$('span').click(deletItem);
+$('button').click(addItem);
+
+function addItem() {
+  const input = $('input').val();
+  $('ul').append(`
+    <li><span>X</span> ${input}</li>
+    `);
+  $('input').val('');
+  $('span').click(deletItem);
+}
+
+function deletItem() {
+  $(this)
+    .parent()
+    .remove();
+}
+
+function strikeOut() {
+  if ($(this).css('color') === 'rgb(128, 128, 128)') {
+    $(this).css({
+      color: 'black',
+      textDecoration: 'none'
+    });
+  } else {
+    $(this).css({
+      color: 'grey',
+      textDecoration: 'line-through'
+    });
+  }
+}
