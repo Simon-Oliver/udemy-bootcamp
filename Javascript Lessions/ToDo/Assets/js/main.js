666883905b305171de7fb883ab0b8ebc6986d492
Simon-Oliver <stauffer.simon@outlook.com>
2018-10-09 23:03:01 +1100

    Simplified code

--- Javascript Lessions/ToDo/Assets/js/main.js
+++ Javascript Lessions/ToDo/Assets/js/main.js
@@ -18,15 +18,5 @@ function deletItem() {
 }
 
 function strikeOut() {
-  if ($(this).css('color') === 'rgb(128, 128, 128)') {
-    $(this).css({
-      color: 'black',
-      textDecoration: 'none'
-    });
-  } else {
-    $(this).css({
-      color: 'grey',
-      textDecoration: 'line-through'
-    });
-  }
+  $(this).toggleClass('completed');
 }
