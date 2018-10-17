a93da6d1e2abb31b75a852dc165606589be90a5c
Simon-Oliver <stauffer.simon@outlook.com>
2018-10-17 22:48:15 +1000

    rout pattern test

--- ExpressApp/app.js
+++ ExpressApp/app.js
@@ -8,9 +8,19 @@ app.get('/', (req, res) => {
 app.get('/bye', (req, res) => {
   res.send('Good Bye');
 });
+
+app.get('/r/:topic/:id/:title', (req, res) => {
+  res.send(`Welcome to the ${req.params.topic} page!!!`);
+  console.log(req.params.topic);
+});
+
 app.get('/dog', (req, res) => {
   console.log('someone looked for dogs!');
   res.send('Dog or cat!!!');
+});
+
+app.get('*', (req, res) => {
+  res.send(`This Page doesn't exist`);
 });
 
 app.listen(3000, () => {
