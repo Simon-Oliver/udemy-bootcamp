6fb1578afe3e8f6706de6f921272eaa87fd46350
Simon-Oliver <stauffer.simon@outlook.com>
2018-10-19 17:07:38 +1000

    partials

--- MoreExpress/app.js
+++ MoreExpress/app.js
@@ -2,12 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('puplic'));
+
 app.get('/', (req, res) => {
   res.render('home.ejs');
 });
-app.get('/fallinlovewith/:thing', (req, res) => {
-  const thing = req.params.thing;
-  res.render('love.ejs', { thing });
+app.get('/posts', (req, res) => {
+  const posts = [
+    { title: 'Post 1', author: 'Max Muster' },
+    { title: 'Post 2', author: 'Helen Example' },
+    { title: 'Post 3', author: 'Yoda' }
+  ];
+  res.render('posts.ejs', { posts });
 });
 
 app.get('*', (req, res) => {
