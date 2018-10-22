b2fe497873fbc534b9494d9cc79d5bfd11d86cce
Simon-Oliver <stauffer.simon@outlook.com>
2018-10-22 22:29:35 +1000

    Excercise with MongoDB

--- PostDemo/app.js
+++ PostDemo/app.js
@@ -1,24 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-const friends = ['Tony', 'Max', 'Anna'];
+mongoose.connect(
+  'mongodb://localhost/test',
+  { useNewUrlParser: true }
+);
+
+const friendSchema = new mongoose.Schema({
+  name: String
+});
+
+const Friend = mongoose.model('friend', friendSchema);
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
 app.get('/friends', (req, res) => {
-  res.render('friends', { friends });
+  Friend.find({}, (err, friends) => {
+    if (err) {
+      console.log('Something went wrong!');
+    } else {
+      res.render('friends', { friends });
+    }
+  });
 });
 
-app.post('/addfriend', (req, res) => {
-  const newFriend = req.body.newFriend;
-  friends.push(newFriend);
+app.post('/friends', (req, res) => {
+  const name = req.body.newFriend;
+  const newFriend = { name };
+  Friend.create(newFriend, (err, newFriend) => {
+    if (err) {
+      console.log('Something went wrong!!!');
+    }
+  });
   res.redirect('/friends');
 });
 
