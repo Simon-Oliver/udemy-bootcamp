364137ef340c14297ae3a1c89656950c5baf08d3
Simon Stauffer <stauffer.simon@outlook.com>
2018-10-29 16:50:31 +1100

    New blogApp

---
+++ blogApp/app.js
@@ -0,0 +1,46 @@
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const express = require('express');
+
+const app = express();
+
+// APP CONFIG
+mongoose.connect(
+  'mongodb://localhost/blogApp',
+  { useNewUrlParser: true },
+);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MONGOOSE/MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now() },
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+// RESTFULL ROUTS
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
+app.get('/blogs', (req, res) => {
+  Blog.find({}, (err, blogPosts) => {
+    if (err) {
+      console.log('something went wrong');
+    } else {
+      res.render('index', { blogPosts });
+    }
+  });
+});
+
+/* app.get('/blogs', (req, res) => {
+  res.render('index');
+}); */
+
+app.listen(3000);
