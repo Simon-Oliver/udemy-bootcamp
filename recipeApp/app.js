ab52a690a4f17735cacdf2f1449cc36a8528a50d
Simon-Oliver <stauffer.simon@outlook.com>
2018-11-06 00:03:48 +1100

    Updated Schema

--- recipeApp/app.js
+++ recipeApp/app.js
@@ -7,6 +7,11 @@ const hbs = require('hbs');
 const app = express();
 
 // APP CONFIG
+mongoose.connect(
+  'mongodb://localhost/recipeApp',
+  { useNewUrlParser: true }
+);
+
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
@@ -17,31 +22,36 @@ hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
 
-const recipes = {
-  recipe1: {
-    title: 'Greek Salad',
-    ingredients: [
-      { ingredient: 'Tomatoes', amount: 5 },
-      { ingredient: 'Cucumber', amount: 3 },
-      { ingredient: 'Olives', amount: '500g' }
-    ],
-    body:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum, perferendis pariatur. Magni id cum nihil non expedita esse, dolorum velit! Quas a ex deleniti et, rerum veritatis adipisci, delectus sunt dolor animi eius ipsam pariatur voluptas vero voluptates dolores consectetur perspiciatis eum ullam cumque, libero excepturi qui odio nobis! Perspiciatis.'
-  },
-  recipe2: {
-    title: 'Second Salad',
-    ingredients: [
-      { ingredient: 'Tomatoes', amount: 5 },
-      { ingredient: 'Cucumber', amount: 3 },
-      { ingredient: 'Olives', amount: '500g' }
-    ],
-    body:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum, perferendis pariatur. Magni id cum nihil non expedita esse, dolorum velit! Quas a ex deleniti et, rerum veritatis adipisci, delectus sunt dolor animi eius ipsam pariatur voluptas vero voluptates dolores consectetur perspiciatis eum ullam cumque, libero excepturi qui odio nobis! Perspiciatis.'
-  }
-};
+// MONGOOSE/MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+  title: String,
+  yield: Number,
+  ingredients: [{ ingredient: String, quantity: Number, measure: String }],
+  body: String
+});
+
+const Recipe = mongoose.model('Recipe', blogSchema);
+
+Recipe.create({
+  title: 'Greek Salad',
+  ingredients: [
+    { ingredient: 'Cucumber', quantity: 2, measure: 'pieces' },
+    { ingredient: 'Olives', quantity: 500, measure: 'g' },
+    { ingredient: 'Feta', quantity: 200, measure: 'g' },
+    { ingredient: 'Salad', quantity: 1, measure: 'head' }
+  ],
+  body:
+    'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum, perferendis pariatur. Magni id cum nihil non expedita esse, dolorum velit! Quas a ex deleniti et, rerum veritatis adipisci, delectus sunt dolor animi eius ipsam pariatur voluptas vero voluptates dolores consectetur perspiciatis eum ullam cumque, libero excepturi qui odio nobis! Perspiciatis.'
+});
 
 app.get('/', (req, res) => {
-  res.render('index', { recipes });
+  Recipe.find({}, (err, recipe) => {
+    if (err) {
+      console.log('Error');
+    } else {
+      res.render('index', { recipe });
+    }
+  });
 });
 
 app.listen(3000);
