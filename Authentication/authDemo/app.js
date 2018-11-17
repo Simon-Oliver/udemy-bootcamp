438e0363d7447bb2483ece74335c913320979f3a
Simon-Oliver <stauffer.simon@outlook.com>
2018-11-17 14:21:30 +1100

    Auth lession init

---
+++ Authentication/authDemo/app.js
@@ -0,0 +1,37 @@
+const hbs = require('hbs');
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const localStrategy = require('passport-local');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const User = require('./models/user');
+
+mongoose.connect('mongodb://localhost/authDemo');
+
+const app = express();
+app.set('view engine', 'hbs');
+app.use(
+  require('express-session')({
+    secret: 'This is a test',
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/secret', (req, res) => {
+  res.render('secret');
+});
+
+app.listen(3000);
